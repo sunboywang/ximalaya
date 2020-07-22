bffad0b146e3a91cc22e7620b1a17ab41ee827ec
sunboywang <153226057@qq.com>
2020-07-22 12:44:39 +0800

    fix android is not running

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@/utils': './src/utils',
+          '@/pages': './src/pages',
+          '@/config': './src/config',
+          '@/components': './src/config',
+          '@/models': './src/models',
+          '@/navigator': './src/navigator',
+          '@/assets': './src/assets',
+        },
+      },
+    ],
+  ],
 };
